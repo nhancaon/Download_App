1eff40cff54121beb3b7350c033cc61000289af5
sage0304 <hiennhanlonggiang@gmail.com>
2024-04-17 23:26:39 +0700

    search, delete toolbar in activity_main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     implementation("it.xabaras.android:recyclerview-swipedecorator:1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
