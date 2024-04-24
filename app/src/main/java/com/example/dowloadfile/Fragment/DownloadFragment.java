6e7098e5ee7a41dfb75f8b2d434e236b257680b1
sage0304 <hiennhanlonggiang@gmail.com>
2024-04-24 23:17:51 +0700

    fix DownloadFragment, create delete in action bar

--- app/src/main/java/com/example/dowloadfile/Fragment/DownloadFragment.java
+++ app/src/main/java/com/example/dowloadfile/Fragment/DownloadFragment.java
@@ -1,6 +1,5 @@
 package com.example.dowloadfile.Fragment;
 
-import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.database.Cursor;
@@ -9,6 +8,8 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -64,10 +65,8 @@ public class DownloadFragment extends Fragment {
         txtDownloadTime = view.findViewById(R.id.txtDownloadTime);
         btnDownloadAndAddImages = view.findViewById(R.id.btnDownloadAndAddImages);
 
-        // Lấy danh sách tất cả các item trực tiếp trong thư mục gốc (bao gồm cả folder và file)
         FirebaseStorage.getInstance().getReference().listAll().addOnSuccessListener(listResult -> {
             for (StorageReference prefix : listResult.getPrefixes()) {
-                // Lấy tên của folder và thêm vào danh sách
                 folderNames.add(prefix.getName());
                 spinnerAdapter.notifyDataSetChanged();
             }
@@ -84,6 +83,7 @@ public class DownloadFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
 
         spinnerFolder.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -104,6 +104,12 @@ public class DownloadFragment extends Fragment {
                 downloadAllImagesInFolder();
             }
         });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.delete_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     private void downloadAllImagesInFolder() {
@@ -191,7 +197,6 @@ public class DownloadFragment extends Fragment {
         }
     }
 
-    // Ngon
     private void loadImagesFromFolder(String folderName) {
         dataList.clear();
 
